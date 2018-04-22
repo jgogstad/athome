62cf433736ec96f0a229b393d918cf67cd6bf577
Jostein Gogstad <jostein.gogstad@tapad.com>
2018-04-22 21:30:23 +0200

    Added "other modifications"

--- src/layers/index.js
+++ src/layers/index.js
@@ -1,8 +1,12 @@
+const YAML = require('yamljs')
+const path = require('path')
 const applicationLayers = require('./application_layers')
 const capsLayer = require('./caps_layer')
 const tabLayer = require('./tab_layer')
 const tabCapsLayer = require('./tabcaps_layer')
 const trainingWheels = require('./training_wheels')
+
+const other = YAML.load(path.join(__dirname, '/other_modifications.yaml'))
 
 module.exports = {
     "title": "At Home",
@@ -12,6 +16,7 @@ module.exports = {
     "manual": "https://github.com/jgogstad/athome",
     "import_url": "karabiner://karabiner/assets/complex_modifications/import?url=https://raw.githubusercontent.com/jgogstad/athome/master/foo.json",
     "rules": [
+        other,
         tabCapsLayer.definition,
         capsLayer.definition,
         tabLayer.definition,
