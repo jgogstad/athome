41c7b66cc0129904ebe608ce89b0f3267163e0a8
Jostein Gogstad <jostein.gogstad@tapad.com>
2018-05-03 22:43:54 +0200

    Removed tabcaps layer, added iterm layer

--- src/layers/application_layers/index.js
+++ src/layers/application_layers/index.js
@@ -2,13 +2,24 @@ const YAML = require('yamljs')
 const path = require('path')
 const utils = require('../../common/utils')
 
-const templates = [{
+// To find the bundle identifier for your application, run
+// `mdls -name kMDItemCFBundleIdentifier /Applications/YourApp.app`
+const templates = [
+  {
     condition: {
         type: 'frontmost_application_if',
         bundle_identifiers: ['^com\\.jetbrains\\.intellij.*']
     },
     rule: YAML.load(path.join(__dirname, '/intellij.yaml'))
-}]
+  },
+  {
+      condition: {
+          type: 'frontmost_application_if',
+          bundle_identifiers: ['^com\\.googlecode\\.iterm2.*']
+      },
+      rule: YAML.load(path.join(__dirname, '/iterm.yaml'))
+  }
+]
 
 module.exports = {
     rules: utils.merge(templates)
--- src/layers/index.js
+++ src/layers/index.js
@@ -3,7 +3,6 @@ const path = require('path')
 const applicationLayers = require('./application_layers')
 const capsLayer = require('./caps_layer')
 const tabLayer = require('./tab_layer')
-const tabCapsLayer = require('./tabcaps_layer')
 const trainingWheels = require('./training_wheels')
 
 const other = YAML.load(path.join(__dirname, '/other_modifications.yaml'))
@@ -17,13 +16,11 @@ module.exports = {
     "import_url": "karabiner://karabiner/assets/complex_modifications/import?url=https://raw.githubusercontent.com/jgogstad/athome/master/foo.json",
     "rules": [
         other,
-        tabCapsLayer.definition,
         capsLayer.definition,
         tabLayer.definition,
         ...trainingWheels.rules,
         ...applicationLayers.rules,
         ...capsLayer.rules,
         ...tabLayer.rules,
-        ...tabCapsLayer.rules
     ]
 }
