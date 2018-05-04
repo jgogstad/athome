5c6b273023777c410ebf4b6760eab9a332752de4
Jostein Gogstad <jostein.gogstad@tapad.com>
2018-05-04 22:43:22 +0200

    Implemented training wheels

--- src/layers/training_wheels/index.js
+++ src/layers/training_wheels/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const rules = [
   { rule: YAML.load(path.join(__dirname, '/training_wheels.yaml')) },
-//  { rule: YAML.load(path.join(__dirname, '/happy_hacker.yaml')) }
+  { rule: YAML.load(path.join(__dirname, '/happy_hacker.yaml')) }
 ]
 
-module.exports = { rules }
+module.exports = { rules: rules.map(r => r.rule ) }
