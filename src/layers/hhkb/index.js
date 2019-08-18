c280a78bcc57995f8294100d40ee896b37775a0e
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-08-18 11:03:45 +0200

    HHKB adjustments

---
+++ src/layers/hhkb/index.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const Utils = require('../../common/utils')
+
+const conditions = [{
+    "type": "variable_if",
+    "name": "caps_lock pressed",
+    "value": 1
+}]
+
+const templates = [
+    { rule: Utils.load(path.join(__dirname, '/navigation.yaml')) },
+    { rule: Utils.load(path.join(__dirname, '/deletion.yaml')) },
+    { rule: Utils.load(path.join(__dirname, '/internationalization/norway.yaml')) },
+    { rule: Utils.load(path.join(__dirname, '/functional.yaml')) },
+    { rule: Utils.load(path.join(__dirname, '/misc.yaml')) },
+    { rule: Utils.load(path.join(__dirname, '/window_management.yaml')) },
+]
+
+const definition = Utils.load(path.join(__dirname, '/layer_definition.yaml'))
+
+module.exports = {
+    conditions,
+    definition,
+    rules: Utils.merge(templates.map(t => ({ ...t, conditions })))
+}
